2552b6bda8bdf13a4a9d13357e2a2e21037af4c3
Matias Crino <matiascrino@gmail.com>
2023-10-04 13:48:40 -0300

    path agregado

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use("/api/events", require("./routes/events"));
 
+
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
 //escuchar peticiones
 
 
