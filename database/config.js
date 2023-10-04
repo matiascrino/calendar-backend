7febc437e95e22d5828a3f5db17a6a4242b26a70
Matias Crino <matiascrino@gmail.com>
2023-10-04 12:18:15 -0300

    make it better

--- database/config.js
+++ database/config.js
@@ -5,7 +5,7 @@ const dbConnection = async() => {
     try{
         await mongoose.connect(process.env.DB_CNN, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            useUnifiedTopology: true
         });
 
         console.log("DB Online")
